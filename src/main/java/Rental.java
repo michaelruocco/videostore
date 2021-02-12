5b815a1f394d790c0170570194e82205b7bfc97e
Michael Ruocco <michael.ruocco@hotmail.com>
2021-02-12 17:21:18 +0000

    cleans up formatting and moves member variables to top of class

--- src/main/java/Rental.java
+++ src/main/java/Rental.java
@@ -1,19 +1,20 @@
 
-public class Rental
-{
-	public Rental (Movie movie, int daysRented) {
-		this.movie 		= movie;
-		this.daysRented = daysRented;
-	}
-	
-	public int getDaysRented () {
-		return daysRented;
-	}
-	
-	public Movie getMovie () {
-		return movie;
-	}
-	
-	private Movie movie;
-	private int daysRented;
+public class Rental {
+
+    private final Movie movie;
+    private final int daysRented;
+
+    public Rental(Movie movie, int daysRented) {
+        this.movie = movie;
+        this.daysRented = daysRented;
+    }
+
+    public int getDaysRented() {
+        return daysRented;
+    }
+
+    public Movie getMovie() {
+        return movie;
+    }
+
 }
