294ce1453399de9c5d541fa3291d2924e513f3f8
Michael Ruocco <michael.ruocco@hotmail.com>
2021-02-12 17:07:03 +0000

    removes use of old style extending test case and uses annotations instead

--- src/test/java/VideoStoreTest.java
+++ src/test/java/VideoStoreTest.java
@@ -1,33 +1,31 @@
-import junit.framework.TestCase;
+import org.junit.Test;
 
-public class VideoStoreTest extends TestCase {
-    
-    private Customer customer;
+import static org.junit.Assert.assertEquals;
 
-    public VideoStoreTest(String name) {
-        super(name);
-    }
+public class VideoStoreTest {
 
-    protected void setUp() {
-        customer = new Customer("Fred");
-    }
+    private final Customer customer = new Customer("Fred");
 
+    @Test
     public void testSingleNewReleaseStatement() {
         customer.addRental(new Rental(new Movie("The Cell", Movie.NEW_RELEASE), 3));
         assertEquals("Rental Record for Fred\n\tThe Cell\t9.0\nYou owed 9.0\nYou earned 2 frequent renter points\n", customer.statement());
     }
 
+    @Test
     public void testDualNewReleaseStatement() {
         customer.addRental(new Rental(new Movie("The Cell", Movie.NEW_RELEASE), 3));
         customer.addRental(new Rental(new Movie("The Tigger Movie", Movie.NEW_RELEASE), 3));
         assertEquals("Rental Record for Fred\n\tThe Cell\t9.0\n\tThe Tigger Movie\t9.0\nYou owed 18.0\nYou earned 4 frequent renter points\n", customer.statement());
     }
 
+    @Test
     public void testSingleChildrensStatement() {
         customer.addRental(new Rental(new Movie("The Tigger Movie", Movie.CHILDRENS), 3));
         assertEquals("Rental Record for Fred\n\tThe Tigger Movie\t1.5\nYou owed 1.5\nYou earned 1 frequent renter points\n", customer.statement());
     }
 
+    @Test
     public void testMultipleRegularStatement() {
         customer.addRental(new Rental(new Movie("Plan 9 from Outer Space", Movie.REGULAR), 1));
         customer.addRental(new Rental(new Movie("8 1/2", Movie.REGULAR), 2));
