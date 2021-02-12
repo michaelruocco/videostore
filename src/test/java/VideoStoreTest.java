4741bddb0d9827c81e8f93a8f33556b8d60d26ff
Michael Ruocco <michael.ruocco@hotmail.com>
2021-02-12 17:11:08 +0000

    updates to junit5

--- src/test/java/VideoStoreTest.java
+++ src/test/java/VideoStoreTest.java
@@ -1,32 +1,32 @@
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class VideoStoreTest {
+class VideoStoreTest {
 
     private final Customer customer = new Customer("Fred");
 
     @Test
-    public void testSingleNewReleaseStatement() {
+    void testSingleNewReleaseStatement() {
         customer.addRental(new Rental(new Movie("The Cell", Movie.NEW_RELEASE), 3));
         assertEquals("Rental Record for Fred\n\tThe Cell\t9.0\nYou owed 9.0\nYou earned 2 frequent renter points\n", customer.statement());
     }
 
     @Test
-    public void testDualNewReleaseStatement() {
+    void testDualNewReleaseStatement() {
         customer.addRental(new Rental(new Movie("The Cell", Movie.NEW_RELEASE), 3));
         customer.addRental(new Rental(new Movie("The Tigger Movie", Movie.NEW_RELEASE), 3));
         assertEquals("Rental Record for Fred\n\tThe Cell\t9.0\n\tThe Tigger Movie\t9.0\nYou owed 18.0\nYou earned 4 frequent renter points\n", customer.statement());
     }
 
     @Test
-    public void testSingleChildrensStatement() {
+    void testSingleChildrensStatement() {
         customer.addRental(new Rental(new Movie("The Tigger Movie", Movie.CHILDRENS), 3));
         assertEquals("Rental Record for Fred\n\tThe Tigger Movie\t1.5\nYou owed 1.5\nYou earned 1 frequent renter points\n", customer.statement());
     }
 
     @Test
-    public void testMultipleRegularStatement() {
+    void testMultipleRegularStatement() {
         customer.addRental(new Rental(new Movie("Plan 9 from Outer Space", Movie.REGULAR), 1));
         customer.addRental(new Rental(new Movie("8 1/2", Movie.REGULAR), 2));
         customer.addRental(new Rental(new Movie("Eraserhead", Movie.REGULAR), 3));
