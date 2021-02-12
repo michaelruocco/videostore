5e197b9fae6d6d447e7dad997d80a7c6a75609ed
Michael Ruocco <michael.ruocco@hotmail.com>
2021-02-12 19:22:19 +0000

    promotes movies in test

--- src/test/java/VideoStoreTest.java
+++ src/test/java/VideoStoreTest.java
@@ -4,13 +4,18 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class VideoStoreTest {
 
-    private final Movie newMovie = new Movie("The Cell", Movie.NEW_RELEASE);
+    private final Movie newReleaseMovie1 = new Movie("New Release 1", Movie.NEW_RELEASE);
+    private final Movie newReleaseMovie2 = new Movie("New Release 2", Movie.NEW_RELEASE);
+    private final Movie childrensMovie = new Movie("Childrens", Movie.CHILDRENS);
+    private final Movie regular1 = new Movie("Regular 1", Movie.REGULAR);
+    private final Movie regular2 = new Movie("Regular 2", Movie.REGULAR);
+    private final Movie regular3 = new Movie("Regular 3", Movie.REGULAR);
 
     private final Customer customer = new Customer("Customer");
 
     @Test
     void testSingleNewReleaseStatementTotals() {
-        customer.addRental(new Rental(newMovie, 3));
+        customer.addRental(new Rental(newReleaseMovie1, 3));
 
         customer.statement();
 
@@ -20,8 +25,8 @@ class VideoStoreTest {
 
     @Test
     void testDualNewReleaseStatementTotals() {
-        customer.addRental(new Rental(newMovie, 3));
-        customer.addRental(new Rental(new Movie("The Tigger Movie", Movie.NEW_RELEASE), 3));
+        customer.addRental(new Rental(newReleaseMovie1, 3));
+        customer.addRental(new Rental(newReleaseMovie2, 3));
 
         customer.statement();
 
@@ -31,7 +36,7 @@ class VideoStoreTest {
 
     @Test
     void testSingleChildrensStatementTotals() {
-        customer.addRental(new Rental(new Movie("The Tigger Movie", Movie.CHILDRENS), 3));
+        customer.addRental(new Rental(childrensMovie, 3));
 
         customer.statement();
 
@@ -41,9 +46,9 @@ class VideoStoreTest {
 
     @Test
     void testMultipleRegularStatementTotals() {
-        customer.addRental(new Rental(new Movie("Plan 9 from Outer Space", Movie.REGULAR), 1));
-        customer.addRental(new Rental(new Movie("8 1/2", Movie.REGULAR), 2));
-        customer.addRental(new Rental(new Movie("Eraserhead", Movie.REGULAR), 3));
+        customer.addRental(new Rental(regular1, 1));
+        customer.addRental(new Rental(regular2, 2));
+        customer.addRental(new Rental(regular3, 3));
 
         customer.statement();
 
@@ -53,14 +58,14 @@ class VideoStoreTest {
 
     @Test
     void testMultipleRegularStatementFormat() {
-        customer.addRental(new Rental(new Movie("Plan 9 from Outer Space", Movie.REGULAR), 1));
-        customer.addRental(new Rental(new Movie("8 1/2", Movie.REGULAR), 2));
-        customer.addRental(new Rental(new Movie("Eraserhead", Movie.REGULAR), 3));
+        customer.addRental(new Rental(regular1, 1));
+        customer.addRental(new Rental(regular2, 2));
+        customer.addRental(new Rental(regular3, 3));
 
         assertThat(customer.statement()).isEqualTo("Rental Record for Customer\n" +
-                "\tPlan 9 from Outer Space\t2.0\n" +
-                "\t8 1/2\t2.0\n" +
-                "\tEraserhead\t3.5\n" +
+                "\tRegular 1\t2.0\n" +
+                "\tRegular 2\t2.0\n" +
+                "\tRegular 3\t3.5\n" +
                 "You owed 7.5\n" +
                 "You earned 3 frequent renter points\n");
     }
