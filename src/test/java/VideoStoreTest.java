a7e721ebd65a290f083fcf4e449e6b82f704dfec
Michael Ruocco <michael.ruocco@hotmail.com>
2021-02-12 17:29:15 +0000

    changes to ensure newline at the end of each line

--- src/test/java/VideoStoreTest.java
+++ src/test/java/VideoStoreTest.java
@@ -10,8 +10,8 @@ class VideoStoreTest {
     void testSingleNewReleaseStatement() {
         customer.addRental(new Rental(new Movie("The Cell", Movie.NEW_RELEASE), 3));
 
-        assertThat(customer.statement()).isEqualTo("Rental Record for Fred\n\t" +
-                "The Cell\t9.0\n" +
+        assertThat(customer.statement()).isEqualTo("Rental Record for Fred\n" +
+                "\tThe Cell\t9.0\n" +
                 "You owed 9.0\n" +
                 "You earned 2 frequent renter points\n");
     }
@@ -21,9 +21,9 @@ class VideoStoreTest {
         customer.addRental(new Rental(new Movie("The Cell", Movie.NEW_RELEASE), 3));
         customer.addRental(new Rental(new Movie("The Tigger Movie", Movie.NEW_RELEASE), 3));
 
-        assertThat(customer.statement()).isEqualTo("Rental Record for Fred\n\t" +
-                "The Cell\t9.0\n\t" +
-                "The Tigger Movie\t9.0\n" +
+        assertThat(customer.statement()).isEqualTo("Rental Record for Fred\n" +
+                "\tThe Cell\t9.0\n" +
+                "\tThe Tigger Movie\t9.0\n" +
                 "You owed 18.0\n" +
                 "You earned 4 frequent renter points\n");
     }
@@ -32,8 +32,8 @@ class VideoStoreTest {
     void testSingleChildrensStatement() {
         customer.addRental(new Rental(new Movie("The Tigger Movie", Movie.CHILDRENS), 3));
 
-        assertThat(customer.statement()).isEqualTo("Rental Record for Fred\n\t" +
-                "The Tigger Movie\t1.5\n" +
+        assertThat(customer.statement()).isEqualTo("Rental Record for Fred\n" +
+                "\tThe Tigger Movie\t1.5\n" +
                 "You owed 1.5\n" +
                 "You earned 1 frequent renter points\n");
     }
@@ -44,8 +44,8 @@ class VideoStoreTest {
         customer.addRental(new Rental(new Movie("8 1/2", Movie.REGULAR), 2));
         customer.addRental(new Rental(new Movie("Eraserhead", Movie.REGULAR), 3));
 
-        assertThat(customer.statement()).isEqualTo("Rental Record for Fred\n\t" +
-                "Plan 9 from Outer Space\t2.0\n" +
+        assertThat(customer.statement()).isEqualTo("Rental Record for Fred\n" +
+                "\tPlan 9 from Outer Space\t2.0\n" +
                 "\t8 1/2\t2.0\n" +
                 "\tEraserhead\t3.5\n" +
                 "You owed 7.5\n" +
